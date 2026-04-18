69534c905d114bfe2b38d5243fa0f1928d146ba6
Jacob Timmis <jacobtimmis07@gmail.com>
2026-04-18 15:28:18 +0100

    make projectile an area2d

--- src/projectile/projectile.gd
+++ src/projectile/projectile.gd
@@ -1,5 +1,7 @@
 class_name Projectile
-extends Node2D
+extends Area2D
+
+signal hit(position: Vector2, normal: Vector2, collider: Object)
 
 @export var speed: float = 140
 @export var life_time: float = 1.0
@@ -9,18 +11,27 @@ extends Node2D
 var direction := Vector2(1, 0)
 var using_projectile_pool := false
 var distance_travelled: float
+var active := false
 
 
 func _enter_tree() -> void:
     distance_travelled = 0
+    active = true
     await get_tree().create_timer(life_time).timeout
     remove()
 
 
 func _process(delta: float) -> void:
+    if not active:
+        return
+
     direction = direction.normalized()
+
     var old_position := position
-    position += direction * speed * delta
+
+    var move_this_frame := direction * speed * delta
+    position += move_this_frame
+
     var position_delta := position - old_position
     distance_travelled += position_delta.length()
     if use_max_distance and distance_travelled > max_distance:
