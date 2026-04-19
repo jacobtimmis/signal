631f3ef3732346f1d7a45bb2fb04148607b2af92
Jacob Timmis <jacobtimmis07@gmail.com>
2026-04-19 23:15:06 +0100

    check is dead before healing

--- src/pickup/pickup.gd
+++ src/pickup/pickup.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-    if body is Hero and body.health_component.is_damaged():
+    if body is Hero and body.health_component.is_damaged() and not body.health_component.is_dead():
         body.health_component.current_health += 50
         body.play_heal_sound()
         queue_free()
