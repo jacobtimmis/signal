fb28691bfa16574d770f6ce559dff41a7258fa9c
Jacob Timmis <jacobtimmis07@gmail.com>
2026-04-19 15:04:29 +0100

    try fix warn
    still needs fix, but not critical

--- src/main.gd
+++ src/main.gd
@@ -11,4 +11,7 @@ func _input(event: InputEvent) -> void:
         event.position *= Vector2(Vector2(subviewport_node.size) / viewport_texture.size)
         event.position -= Vector2(subviewport_node.size) / 2
 
-    subviewport_node.push_input(event)
+    if is_inside_tree() and subviewport_node.is_inside_tree():
+        # there is an error here when scene is reloaded
+        # not critical
+        subviewport_node.push_input(event)
